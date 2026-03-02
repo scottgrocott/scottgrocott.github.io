1ee865c26aa8ee4a6c90023b0bc697a3774a0653
Scott Grocott <sagrocott@protonmail.com>
2026-03-01 17:40:00 -0800

    added terrain model for drone wars

--- drone_wars/aframe/js/@recast-navigation/core/dist/serdes/export.d.ts
+++
@@ -1,4 +0,0 @@
-import type { NavMesh } from '../nav-mesh';
-import type { TileCache } from '../tile-cache';
-export declare const exportNavMesh: (navMesh: NavMesh) => Uint8Array;
-export declare const exportTileCache: (navMesh: NavMesh, tileCache: TileCache) => Uint8Array;
